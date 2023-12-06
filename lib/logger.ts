519543bd789dade89fabbf8a310fadce2b2c5657
Tanner B. Hess Webber <tb@hesswebber.com>
2023-12-06 00:31:57 -0500

    feat(logger): adds progress logging

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,9 +1,15 @@
 import chalk from "chalk";
+import inquirer from "inquirer";
 
-import { ZERO } from "./constants";
+import { ONE, ZERO } from "./constants";
 
-export function log<T>(value: T, ...values: unknown[]): T {
-  console.log(value, ...values);
+const ui = new inquirer.ui.BottomBar();
+
+export function log<T>(value: T): T {
+  const spacing = 2;
+  ui.log.write(
+    typeof value === "object" ? JSON.stringify(value, null, spacing) : value,
+  );
   return value;
 }
 
@@ -38,13 +44,57 @@ ${lines
 \\${"*".repeat(maxLineLength + padding)}/
 `;
 
-  console.log(chalk.bold.blue(message));
+  ui.log.write(chalk.bold.blue(message));
 
   return message;
+}
+
+function* spinnerGenerator(): Generator<string, string, boolean> {
+  const symbols = ["/", "-", "\\", "|"];
+  let index = ZERO;
+  let shouldRun = true;
+  while (shouldRun) {
+    shouldRun = yield symbols[(index += ONE) % symbols.length];
+  }
+
+  return "█";
+}
+
+function makeProgressLogger(total: number): (value: number) => void {
+  const { columns } = process.stdout;
+  const spinner = spinnerGenerator();
+  function getProgressString(value: number): string {
+    const progressRatio = value > ZERO ? value / total : ZERO;
+    const progressString = `${
+      spinner.next(progressRatio !== ONE).value
+    } - ${value}/${total}`;
+    const emptyBracketCount = 2;
+    const bracketCount =
+      progressRatio < ONE / columns
+        ? emptyBracketCount
+        : progressRatio === ONE
+          ? ZERO
+          : ONE;
+    const extraCharacterCount = progressString.length + bracketCount;
+    const availableColumns = columns - extraCharacterCount;
+    const filledColumnCount = Math.floor(progressRatio * availableColumns);
+    const emptyColumnCount = availableColumns - filledColumnCount;
+
+    return `${bracketCount === emptyBracketCount ? "[" : ""}${"█".repeat(
+      filledColumnCount,
+    )}${" ".repeat(emptyColumnCount)}${
+      bracketCount === ONE ? "]" : ""
+    }${progressString}`;
+  }
+
+  return (value: number) => {
+    ui.updateBottomBar(chalk.white.bold(getProgressString(value)));
+  };
 }
 
 export const logger = {
   log,
   error,
   frame,
+  makeProgressLogger,
 };
