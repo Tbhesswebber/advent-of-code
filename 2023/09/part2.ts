86e39c82da97047fb35f2da067899657dbf98acd
Tanner B. Hess Webber <tb@hesswebber.com>
2023-12-09 00:28:21 -0500

    solve(2023): adds basic solution for day 09

---
+++ 2023/09/part2.ts
@@ -0,0 +1,46 @@
+import { ONE, ZERO } from "@lib/constants";
+import { arraySum } from "@lib/math";
+
+type DayInputs = number[][];
+
+export default function main(inputs: DayInputs): number {
+  const nextValues: number[] = [];
+
+  for (let index = ZERO; index < inputs.length; index += ONE) {
+    const transforms: number[][] = [inputs[index]];
+    let latestTransform = transforms.at(-ONE);
+
+    while (
+      latestTransform !== undefined &&
+      latestTransform.some((value) => value !== ZERO)
+    ) {
+      transforms.push(
+        latestTransform.reduce<number[]>(
+          (agg, current, currentIndex, all): number[] => {
+            if (currentIndex === ZERO) return agg;
+            return [...agg, current - all[currentIndex - ONE]];
+          },
+          [],
+        ),
+      );
+      latestTransform = transforms.at(-ONE);
+    }
+
+    const value = transforms.reduceRight(
+      (previousTransformNewValue, currentTransform) => {
+        const addend1 = currentTransform[ZERO];
+        const addend2 = previousTransformNewValue;
+        return addend1 - addend2;
+      },
+      ZERO,
+    );
+
+    nextValues.push(value);
+  }
+
+  return arraySum(nextValues);
+}
+
+export function transformInput(inputs: string[]): DayInputs {
+  return inputs.filter(Boolean).map((line) => line.split(" ").map(Number));
+}
