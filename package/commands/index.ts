9d13e94c287a4e272603814d54d5e056c611f3da
Tanner B. Hess Webber <tb@hesswebber.com>
2023-12-13 23:50:10 -0500

    feat(commands): adds open command

--- package/commands/index.ts
+++ package/commands/index.ts
@@ -2,3 +2,4 @@ export { runCommand } from "./run";
 export { initCommand } from "./init";
 export { testCommand } from "./test";
 export { solveCommand } from "./solve";
+export { openCommand } from "./open";
---
+++ package/commands/open.ts
@@ -0,0 +1,80 @@
+import { openInEditor } from "bun";
+import { Command } from "commander";
+import inquirer from "inquirer";
+
+import { logger } from "@lib/logger";
+
+import { AoC } from "../api";
+import { DECEMBER } from "../libs/constants";
+import { dayArgument } from "../libs/inquisitor/arguments";
+import { dayOption, yearOption } from "../libs/inquisitor/options";
+import { datePrompts } from "../libs/inquisitor/prompts";
+import { openBrowser } from "../libs/node/child-process";
+import { ChristmasError } from "../libs/oops/christmas-error";
+
+import type { DatePrompt } from "../libs/inquisitor/prompts";
+
+interface OpenPrompt extends DatePrompt {
+  editor?: boolean;
+  web?: boolean;
+}
+
+export const openCommand = new Command("open")
+  .version("1.0.0")
+  .description("open all or some of the useful apps for the day")
+  .addOption(yearOption)
+  .addOption(dayOption)
+  .option("-w, --web", "open the default browser to the problem for the day")
+  .option("-e, --editor", "open the default IDE to the problem for the day")
+  .addArgument(dayArgument)
+  .action(async (dayInput: number | undefined, rawOptions: OpenPrompt) => {
+    const rawOptionCopy = { ...rawOptions };
+    if (dayInput !== undefined) {
+      rawOptionCopy.year = new Date().getFullYear();
+      rawOptionCopy.day = new Date().getDate() + dayInput;
+    }
+
+    const optionsWithDate = await datePrompts(rawOptionCopy);
+
+    const {
+      year,
+      day,
+      web: shouldOpenBrowser,
+      editor: shouldOpenIde,
+    } = await inquirer.prompt<Required<OpenPrompt>>(
+      [
+        {
+          name: "web",
+          message: "Open a browser?",
+          type: "confirm",
+          default: true,
+          when: rawOptionCopy.editor !== true,
+        },
+        {
+          name: "editor",
+          message: "Open your IDE?",
+          type: "confirm",
+          default: true,
+          when: rawOptionCopy.web !== true,
+        },
+      ],
+      optionsWithDate,
+    );
+
+    try {
+      const api = new AoC(new Date(year, DECEMBER, day));
+
+      if (shouldOpenIde) {
+        openInEditor(api.part1SolutionPath);
+      }
+      if (shouldOpenBrowser) {
+        openBrowser(api.problemUrl);
+      }
+    } catch (error) {
+      if (error instanceof ChristmasError) {
+        logger.error(error.message);
+      } else {
+        logger.error(error);
+      }
+    }
+  });
