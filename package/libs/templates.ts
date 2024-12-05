23f928ff97d929a8dfe38fdb0a49581887430515
Tanner B. Hess Webber <tanner.hesswebber@addepar.com>
2024-12-05 01:07:36 -0500

    fix(template): infers DayInput type from processing return type

--- package/libs/templates.ts
+++ package/libs/templates.ts
@@ -2,12 +2,12 @@ export const emptyFileTemplate = "";
 
 export const emptyJsonFileTemplate = "{}";
 
-export const solutionFileTemplate = `type DayInputs = string[];
+export const solutionFileTemplate = `type DayInputs = ReturnType<typeof transformInput>;
 
 export default function main(inputs: DayInputs): number {
   throw new Error("Not yet implemented");
 }
 
-export function transformInput(inputs: string[]): DayInputs {
+export function transformInput(inputs: string[]) {
     return inputs;
 }`;
