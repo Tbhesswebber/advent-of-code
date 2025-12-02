3a9d349e2835705c4c9f6d4b6531570169893248
Tanner B. Hess Webber <tanner.hesswebber@addepar.com>
2025-12-01 20:17:12 -0500

    chore(constants): adds missing constants for tens and hundreds

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,7 @@
 export const ZERO = 0;
 export const ONE = 1;
+export const TEN = 10;
+export const HUNDRED = 100;
 export const THOUSAND = 1000;
 export const MILLION = 1_000_000;
 export const BILLION = 1_000_000_000;
